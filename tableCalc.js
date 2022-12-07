2b533f78f805a0e9afbaa0dec8f42ac1de4169c0
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-12-07 14:58:34 -0300

    finalizando parte 1 do exercicio

---
+++ tableCalc.js
@@ -0,0 +1,14 @@
+var table = [];
+
+function TableCalc(num) {
+    for (let index = 0; index <= 10; index++) {
+        table.push(num * index);
+    }
+    for (let index = 0; index <= 10; index++) {
+        table.push((num+1) * index);
+        table.push((num+2) * index);
+    }
+    console.log(table);
+}
+
+TableCalc(1);
