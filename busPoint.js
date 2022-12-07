a3a11f83b3c4291e22622b509484ee3a9dc93baf
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-12-07 19:52:32 -0300

    Finalizando parte dois do exercicio

---
+++ busPoint.js
@@ -0,0 +1,12 @@
+var points = [[7,0],[3,5],[27,10]];
+
+function busPoint(points) {
+    let passengers = 0;
+    points.forEach(x => {
+        passengers = passengers + x[0] - x[1];
+    });
+    console.log(passengers);
+    return passengers;
+}
+
+busPoint(points);
